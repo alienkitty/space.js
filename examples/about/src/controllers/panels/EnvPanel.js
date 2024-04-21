2599901f0591d71c85fd952b767d18575d749680
Patrick Schroen <ps@ufotechnologies.com>
2024-04-21 11:40:15 -0400

    Add environmentIntensity to environment panel

--- examples/about/src/controllers/panels/EnvPanel.js
+++ examples/about/src/controllers/panels/EnvPanel.js
@@ -10,12 +10,30 @@ export class EnvPanel extends Panel {
     }
 
     initPanel() {
+        const scene = this.scene;
+
         const items = [
             {
                 type: 'divider'
             }
             // TODO: Texture thumbnails
         ];
+
+        if (scene.environment) {
+            items.push(
+                {
+                    type: 'slider',
+                    name: 'Int',
+                    min: 0,
+                    max: 10,
+                    step: 0.1,
+                    value: scene.environmentIntensity,
+                    callback: value => {
+                        scene.environmentIntensity = value;
+                    }
+                }
+            );
+        }
 
         items.forEach(data => {
             this.add(new PanelItem(data));
