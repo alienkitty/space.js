13f9ef267aeba71af0ffdb9dde9d141d12436598
Patrick Schroen <ps@ufotechnologies.com>
2025-01-22 09:01:45 -0500

    Graph updates

--- src/ui/Graph.js
+++ src/ui/Graph.js
@@ -116,9 +116,12 @@ export class Graph extends Interface {
 
         this.props = {
             alpha: 0,
-            handleAlpha: 0,
             yMultiplier: 0,
             progress: 0
+        };
+
+        this.handleProps = {
+            alpha: 0
         };
 
         this.init();
@@ -139,8 +142,6 @@ export class Graph extends Interface {
         }
 
         this.setSize(this.width, this.height);
-
-        this.addListeners();
     }
 
     init() {
@@ -256,7 +257,7 @@ export class Graph extends Interface {
 
     initMarkers() {
         this.markers.forEach(data => {
-            this.addMarker(data, 500);
+            this.addMarker(data);
         });
     }
 
@@ -267,6 +268,11 @@ export class Graph extends Interface {
             window.addEventListener('pointerdown', this.onPointerDown);
             window.addEventListener('pointermove', this.onPointerMove);
         }
+
+        this.items.forEach(item => {
+            item.events.on('update', this.onMarkerUpdate);
+            item.events.on('click', this.onMarkerClick);
+        });
     }
 
     removeListeners() {
@@ -346,11 +352,11 @@ export class Graph extends Interface {
     onPointerUp = e => {
         window.removeEventListener('pointerup', this.onPointerUp);
 
-        if (performance.now() - this.lastTime > 250 || this.delta.length() > 50) {
+        if (e.target !== this.element) {
             return;
         }
 
-        if (e.target !== this.element) {
+        if (performance.now() - this.lastTime > 250 || this.delta.length() > 50) {
             return;
         }
 
@@ -463,24 +469,25 @@ export class Graph extends Interface {
         this.update();
     }
 
-    addMarker([x, name], delay = 0) {
+    addMarker([x, name]) {
         const item = new GraphMarker({ name, noDrag: this.noMarkerDrag });
         item.css({ top: -12 });
         item.x = x;
         item.multiplier = 0;
-        item.events.on('update', this.onMarkerUpdate);
-        item.events.on('click', this.onMarkerClick);
         this.add(item);
 
         this.items.push(item);
 
-        tween(item, { multiplier: 1 }, 400, 'easeOutCubic', delay, null, () => {
-            this.needsUpdate = true;
-
-            item.css({ opacity: item.multiplier });
-        });
-
         if (this.initialized) {
+            item.events.on('update', this.onMarkerUpdate);
+            item.events.on('click', this.onMarkerClick);
+
+            tween(item, { multiplier: 1 }, 400, 'easeOutCubic', null, () => {
+                this.needsUpdate = true;
+
+                item.css({ opacity: item.multiplier });
+            });
+
             Stage.events.emit('marker', { type: 'add', item, target: this });
         }
     }
@@ -523,10 +530,6 @@ export class Graph extends Interface {
         if (this.needsUpdate || this.hoveredIn || this.isDragging) {
             this.drawGraph();
             this.needsUpdate = false;
-        }
-
-        if (!this.initialized) {
-            this.initialized = true;
         }
     }
 
@@ -610,10 +613,10 @@ export class Graph extends Interface {
                 y = h - value * this.rangeHeight - 1;
             }
 
-            if (this.props.handleAlpha < 0.001) {
+            if (this.handleProps.alpha < 0.001) {
                 this.context.globalAlpha = 0;
             } else {
-                this.context.globalAlpha = this.props.handleAlpha;
+                this.context.globalAlpha = this.handleProps.alpha;
             }
 
             this.context.lineWidth = 1;
@@ -700,43 +703,33 @@ export class Graph extends Interface {
         }
     }
 
-    hoverIn(force) {
-        if (this.hoveredIn && !force) {
-            return;
-        }
+    hoverIn() {
+        clearTween(this.handleProps);
 
-        this.hoveredIn = true;
-
-        clearTween(this.props);
-
-        tween(this.props, { handleAlpha: 1 }, 275, 'easeInOutCubic', null, () => {
+        tween(this.handleProps, { alpha: 1 }, 275, 'easeInOutCubic', null, () => {
             this.needsUpdate = true;
         });
 
         this.info.clearTween();
         this.info.visible();
         this.info.tween({ opacity: 1 }, 275, 'easeInOutCubic');
+
+        this.hoveredIn = true;
     }
 
     hoverOut(fast) {
-        if (!this.hoveredIn) {
-            return;
-        }
-
-        this.hoveredIn = false;
-
-        clearTween(this.props);
+        clearTween(this.handleProps);
 
         this.info.clearTween();
 
         if (fast) {
-            this.props.handleAlpha = 0;
+            this.handleProps.alpha = 0;
             this.needsUpdate = true;
 
             this.info.css({ opacity: 0 });
             this.info.invisible();
         } else {
-            tween(this.props, { handleAlpha: 0 }, 275, 'easeInOutCubic', null, () => {
+            tween(this.handleProps, { alpha: 0 }, 275, 'easeInOutCubic', null, () => {
                 this.needsUpdate = true;
             });
 
@@ -744,10 +737,20 @@ export class Graph extends Interface {
                 this.info.invisible();
             });
         }
+
+        this.hoveredIn = false;
     }
 
     animateIn(fast) {
+        this.addListeners();
+
         clearTween(this.props);
+
+        if (!this.initialized) {
+            this.initialized = true;
+
+            this.update();
+        }
 
         if (fast) {
             this.props.alpha = 1;
@@ -760,7 +763,7 @@ export class Graph extends Interface {
             this.update();
 
             if (this.hoveredIn) {
-                this.hoverIn(true);
+                this.hoverIn();
             }
         } else {
             this.props.alpha = 0;
@@ -774,7 +777,17 @@ export class Graph extends Interface {
                     this.animatedIn = true;
 
                     if (this.hoveredIn) {
-                        this.hoverIn(true);
+                        this.hoverIn();
+                    }
+
+                    if (!this.noMarker) {
+                        this.items.forEach(item => {
+                            tween(item, { multiplier: 1 }, 400, 'easeOutCubic', null, () => {
+                                this.needsUpdate = true;
+
+                                item.css({ opacity: item.multiplier });
+                            });
+                        });
                     }
                 }, () => {
                     this.needsUpdate = true;
@@ -786,9 +799,13 @@ export class Graph extends Interface {
     }
 
     animateOut() {
+        this.removeListeners();
+
         clearTween(this.props);
 
         this.animatedIn = false;
+
+        this.hoverOut(true);
 
         tween(this.props, { alpha: 0 }, 300, 'easeOutSine');
 
@@ -797,7 +814,9 @@ export class Graph extends Interface {
 
             if (!this.noMarker) {
                 this.items.forEach(item => {
-                    item.css({ opacity: this.props.yMultiplier });
+                    item.multiplier = this.props.yMultiplier;
+
+                    item.css({ opacity: item.multiplier });
                 });
             }
         });
