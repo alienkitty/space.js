f2e44744f9dd98a37a9e29c2849ac86aa641844d
Patrick Schroen <ps@ufotechnologies.com>
2025-01-24 14:11:59 -0500

    UI house cleaning

--- src/ui/GraphSegments.js
+++ src/ui/GraphSegments.js
@@ -51,7 +51,7 @@ export class GraphSegments extends Interface {
         noMarkerDrag = false,
         noGradient = false
     } = {}) {
-        super('.graph-segments');
+        super('.graph');
 
         this.value = value;
         this.ghost = ghost;
