42757115171563c7e217677e8b5c6a521c67135f
Patrick Schroen <ps@ufotechnologies.com>
2025-01-29 08:50:53 -0500

    Graph setGhostArray() uses array length as default

--- src/panels/PanelGraph.js
+++ src/panels/PanelGraph.js
@@ -369,18 +369,6 @@ export class PanelGraph extends Interface {
         this.needsUpdate = true;
     }
 
-    setGhostArray(value) {
-        if (Array.isArray(value)) {
-            this.ghostArray = value;
-        } else {
-            this.ghostArray = new Array(this.resolution).fill(0);
-        }
-
-        this.needsUpdate = true;
-
-        this.update();
-    }
-
     setArray(value) {
         if (Array.isArray(value)) {
             if (!this.callback) {
@@ -398,6 +386,18 @@ export class PanelGraph extends Interface {
             this.pathData = '';
             this.graphNeedsUpdate = true;
         }
+
+        this.update();
+    }
+
+    setGhostArray(value) {
+        if (Array.isArray(value)) {
+            this.ghostArray = value;
+        } else {
+            this.ghostArray = new Array(this.array.length).fill(0);
+        }
+
+        this.needsUpdate = true;
 
         this.update();
     }
