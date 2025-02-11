6b27fde6b3247438a7da58e6bcb3de76e5535ed8
Patrick Schroen <ps@ufotechnologies.com>
2025-02-11 09:42:51 -0500

    Bezier values with no loss of precision

--- src/path/functions/BezierValues.js
+++ src/path/functions/BezierValues.js
@@ -5,16 +5,10 @@
  */
 
 // Legendre-Gauss abscissae with n=2 (xi values, defined at i=n as the roots of the nth order Legendre polynomial Pn(x))
-export const tValues = [
-    -0.5773502691896257645091487805019574556476,
-    0.5773502691896257645091487805019574556476
-];
+export const tValues = [-0.5773502691896257, 0.5773502691896257];
 
 // Legendre-Gauss weights with n=2 (wi values, defined by a function linked to in the Bezier primer article)
-export const cValues = [
-    1.0,
-    1.0
-];
+export const cValues = [1, 1];
 
 // LUT for binomial coefficient arrays per curve order n
 export const binomialCoefficients = [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]];
