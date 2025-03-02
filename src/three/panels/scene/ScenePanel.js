5fda0c4ead5a04372f652e8be2b31a14ddef4d84
Patrick Schroen <ps@ufotechnologies.com>
2025-03-02 08:53:36 -0500

    ScenePanel background properties support

--- src/three/panels/scene/ScenePanel.js
+++ src/three/panels/scene/ScenePanel.js
@@ -29,27 +29,104 @@ export class ScenePanel extends Panel {
             // TODO: Texture thumbnails
         ];
 
-        if (scene.background && scene.background.isColor) {
+        if (scene.background) {
             items.push(
                 {
                     type: 'divider'
-                },
-                {
-                    type: 'color',
-                    value: scene.background,
-                    callback: value => {
-                        scene.background.copy(value);
-
-                        this.setInvert(value);
-                    }
                 }
             );
+
+            if (scene.background.isColor) {
+                items.push(
+                    {
+                        type: 'color',
+                        value: scene.background,
+                        callback: value => {
+                            scene.background.copy(value);
+
+                            this.setInvert(value);
+                        }
+                    }
+                );
+            } else if (scene.background.isTexture) {
+                items.push(
+                    {
+                        type: 'slider',
+                        name: 'Blur',
+                        min: 0,
+                        max: 1,
+                        step: 0.01,
+                        value: scene.backgroundBlurriness,
+                        callback: value => {
+                            scene.backgroundBlurriness = value;
+                        }
+                    },
+                    {
+                        type: 'slider',
+                        name: 'Int',
+                        min: 0,
+                        max: 10,
+                        step: 0.1,
+                        value: scene.backgroundIntensity,
+                        callback: value => {
+                            scene.backgroundIntensity = value;
+                        }
+                    },
+                    {
+                        type: 'slider',
+                        name: 'Rotate X',
+                        min: 0,
+                        max: 360,
+                        step: 1,
+                        value: MathUtils.radToDeg(scene.backgroundRotation.x),
+                        callback: value => {
+                            value = MathUtils.degToRad(value);
+                            scene.backgroundRotation.x = value;
+                        }
+                    },
+                    {
+                        type: 'slider',
+                        name: 'Rotate Y',
+                        min: 0,
+                        max: 360,
+                        step: 1,
+                        value: MathUtils.radToDeg(scene.backgroundRotation.y),
+                        callback: value => {
+                            value = MathUtils.degToRad(value);
+                            scene.backgroundRotation.y = value;
+                        }
+                    },
+                    {
+                        type: 'slider',
+                        name: 'Rotate Z',
+                        min: 0,
+                        max: 360,
+                        step: 1,
+                        value: MathUtils.radToDeg(scene.backgroundRotation.z),
+                        callback: value => {
+                            value = MathUtils.degToRad(value);
+                            scene.backgroundRotation.z = value;
+                        }
+                    }
+                );
+            }
         }
 
         if (scene.environment) {
             items.push(
                 {
                     type: 'divider'
+                },
+                {
+                    type: 'slider',
+                    name: 'Int',
+                    min: 0,
+                    max: 10,
+                    step: 0.1,
+                    value: scene.environmentIntensity,
+                    callback: value => {
+                        scene.environmentIntensity = value;
+                    }
                 },
                 {
                     type: 'slider',
@@ -85,17 +162,6 @@ export class ScenePanel extends Panel {
                     callback: value => {
                         value = MathUtils.degToRad(value);
                         scene.environmentRotation.z = value;
-                    }
-                },
-                {
-                    type: 'slider',
-                    name: 'Int',
-                    min: 0,
-                    max: 10,
-                    step: 0.1,
-                    value: scene.environmentIntensity,
-                    callback: value => {
-                        scene.environmentIntensity = value;
                     }
                 }
             );
